20673ae1ef4cfc5bd7b196fa6f62a59886dea1af
kingseungwooya <ksw_0505@nate.com>
2023-04-16 22:38:54 +0900

    사진을 넣어보고 여러 style을 사용해봄

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, //  우측 상단 디버그 라벨 없애기 
       title: 'First app',
       theme: ThemeData(primarySwatch: Colors.lightBlue),
       home: Grade(),
@@ -23,37 +24,116 @@ class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[700],
-      appBar: AppBar(
-        title: Text('BBANTO'),
-        centerTitle: true,
-        backgroundColor: Colors.amber[500],
-        elevation: 0.0, // appbar 밑에 그림자 없애줌
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            Text('Name'
-            ,style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0
-              ),),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text('BBANTO',
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold
-            ),
-            ),
-          ],
+        backgroundColor: Colors.amber[700],
+        appBar: AppBar(
+          title: Text('BBANTO'),
+          centerTitle: true,
+          backgroundColor: Colors.amber[500],
+          elevation: 0.0, // appbar 밑에 그림자 없애줌
         ),
-      )
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('pokemon.png'),
+                  radius: 60.0,
+                ),
+              ),
+              Divider(
+                height: 60.0,
+                color: Colors.grey[850],
+                thickness: 0.5,
+                endIndent: 30.0,
+              ),
+              Text(
+                'Name',
+                style: TextStyle(color: Colors.white, letterSpacing: 2.0),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'BBANTO',
+                style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                'BBANTO Power Level',
+                style: TextStyle(color: Colors.white, letterSpacing: 2.0),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                '10',
+                style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                children: const <Widget> [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text('using lightsaber',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0
+                  ),)
+                ],
+              ),
+              Row(
+                children: const <Widget> [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text('face hero tattoo',
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        letterSpacing: 1.0
+                    ),)
+                ],
+              ),
+              Row(
+                children: const <Widget> [
+                  Icon(Icons.check_circle_outline),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text('fire flames',
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        letterSpacing: 1.0
+                    ),)
+                ],
+              ),
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('ball.png'),
+                  radius: 40.0,
+                  backgroundColor: Colors.amber[700],
+                ),
+              )
+            ],
+          ),
+        )
     );
   }
 }
